916c622283b2d9c2d12acd8ebfcecf3a3776bb26
Klaus Dieter Kupper <klausdk@pop-os.localdomain>
2022-06-23 20:22:14 -0300

    mensagens filtradas(privadas e status)

--- app.js
+++ app.js
@@ -3,17 +3,38 @@ import cors from 'cors';
 import dayjs from 'dayjs';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
-import { useLinkClickHandler } from 'react-router-dom';
+import chalk from "chalk";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 const mongoClient = new MongoClient(process.env.MONGO_URL);//uri
 let db;
+
+setInterval(isOnline, 15000);
+
+async function isOnline(){
+    
+    try {
+	    const participants = await db.collection('participants').find().toArray();
+		for(let i=0;i<participants.length;i++){
+            if((Date.now()-participants[i].lastStatus)>=10000){
+                const message= {from: participants[i].name, to: 'Todos', text: 'sai da sala...', type: 'status', time: dayjs().format("HH:mm:ss")};
+                db.collection('messages').insertOne(message);
+                db.collection('participants').deleteOne(participants[i]);
+            }
+        }	
+
+	} catch (error) {
+	  res.status(500).send('A culpa foi do estagiário')
+	}
+   
+}
 
 mongoClient.connect().then(()=> {
     db = mongoClient.db("batepapouol");
@@ -70,11 +91,9 @@ app.get("/messages", (req, res) => {
     const promise = db.collection('messages').find().toArray();
 
     promise.then(messages => {
-        messages.reverse();
-        const userMessages = messages.filter(item => {
-            if (item.from === user ||item.type === 'message' || item.type === 'status') {
-                return true;
-            } else if (item.type === 'private_message' && (item.to === user || item.to === "Todos")) {
+        //messages.reverse();
+        const userMessages = messages.filter(message=> {
+            if (message.from === user || message.to === "Todos" || message.type === 'status'||(message.type === 'private_message' && message.to === user )) {
                 return true;
             } else {
                 return false;
@@ -96,7 +115,35 @@ app.post("/status", (req, res) => {
        if(userdata){
         //const participant = { $set:{ lastStatus: Date.now()}};
         db.collection('participants').updateOne({name:user},{ $set:{ lastStatus: Date.now()}});
-        res.sendStatus(201);
+        res.sendStatus(200);
+       }else{
+        res.sendStatus(404);
+       }
+    });
+});
+
+app.delete("/messages/:ID_DA_MENSAGEM", (req, res) => {
+    const { user } = req.headers;
+    const promise = db.collection('participants').find({name:user}).toArray();
+    promise.then(userdata => {
+       if(userdata){
+        //const participant = { $set:{ lastStatus: Date.now()}};
+        db.collection('participants').updateOne({name:user},{ $set:{ lastStatus: Date.now()}});
+        res.sendStatus(200);
+       }else{
+        res.sendStatus(404);
+       }
+    });
+});
+
+app.put("/messages/:ID_DA_MENSAGEM", (req, res) => {
+    const { user } = req.headers;
+    const promise = db.collection('participants').find({name:user}).toArray();
+    promise.then(userdata => {
+       if(userdata){
+        //const participant = { $set:{ lastStatus: Date.now()}};
+        db.collection('participants').updateOne({name:user},{ $set:{ lastStatus: Date.now()}});
+        res.sendStatus(200);
        }else{
         res.sendStatus(404);
        }
