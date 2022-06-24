15f53200f8d3c73e122cd521b03b28f771b2ebaf
Klaus Dieter Kupper <klausdk@pop-os.localdomain>
2022-06-24 16:16:04 -0300

    joi e verificações

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ import dayjs from 'dayjs';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import chalk from "chalk";
+import joi from "joi";
+
+const nameSchema = joi.object({
+    name: joi.string().required()
+});
+
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required().valid(...['message','private_message'])
+});
 
 dotenv.config();
 
@@ -32,20 +43,33 @@ async function isOnline(){
 
 	} catch (error) {
 	  res.status(500).send('A culpa foi do estagiário')
-	}
-   
+	}  
 }
 
 mongoClient.connect().then(()=> {
     db = mongoClient.db("batepapouol");
 });
 
-app.post("/participants", (req, res) => {
+app.post("/participants", async (req, res) => {
 
-    //joi // name deve ser strings não vazio // Impeça o cadastro de um nome que já está sendo utilizado (caso exista, retornar status 409)
+    const validation = nameSchema.validate(req.body, { abortEarly: true });
 
+    if (validation.error) {
+        res.sendStatus(422);
+        return;
+    }
+  
     const { name } = req.body;
+   
+    const usuarioIsTaken = await db.collection('participants').find({ name }).toArray();
+    
+    if(usuarioIsTaken.length>0){
+        res.sendStatus(409);
+        return;
+    }
+
     const participant = { name, lastStatus: Date.now()};
+
     const message = {
         from: name, 
         to: 'Todos', 
@@ -65,13 +89,26 @@ app.get("/participants", (req, res) => {
     promise.then(participants => res.send(participants));
 });
 
-app.post("/messages", (req, res) => {
+app.post("/messages", async (req, res) => {
 
-    //joi
+    const validation = messageSchema.validate(req.body, { abortEarly: true });
+
+    if (validation.error) {
+        res.sendStatus(422);
+        return;
+    }
 
     const { to, text, type } = req.body;
+    
     const from = req.headers.user;
     
+    const isValidUser = await db.collection('messages').find({ from:from }).toArray();
+    
+    if(!(isValidUser.length>0)){
+        res.sendStatus(409);
+        return;
+    }
+
     const message = {
         from, 
         to, 
@@ -91,7 +128,7 @@ app.get("/messages", (req, res) => {
     const promise = db.collection('messages').find().toArray();
 
     promise.then(messages => {
-        //messages.reverse();
+
         const userMessages = messages.filter(message=> {
             if (message.from === user || message.to === "Todos" || message.type === 'status'||(message.type === 'private_message' && message.to === user )) {
                 return true;
@@ -100,10 +137,10 @@ app.get("/messages", (req, res) => {
             }
         });
 
-        if (!limit || userMessages.length < limit) {
+        if (userMessages.length < limit) {
             res.send(userMessages);
         } else {
-            res.send(userMessages.slice(0 , limit));
+            res.send(userMessages.slice(userMessages.length-limit-1, userMessages.length));
         }
     });
 });
@@ -122,32 +159,32 @@ app.post("/status", (req, res) => {
     });
 });
 
-app.delete("/messages/:ID_DA_MENSAGEM", (req, res) => {
-    const { user } = req.headers;
-    const promise = db.collection('participants').find({name:user}).toArray();
-    promise.then(userdata => {
-       if(userdata){
-        //const participant = { $set:{ lastStatus: Date.now()}};
-        db.collection('participants').updateOne({name:user},{ $set:{ lastStatus: Date.now()}});
-        res.sendStatus(200);
-       }else{
-        res.sendStatus(404);
-       }
-    });
-});
+// app.delete("/messages/:ID_DA_MENSAGEM", (req, res) => {
+//     const { user } = req.headers;
+//     const promise = db.collection('participants').find({name:user}).toArray();
+//     promise.then(userdata => {
+//        if(userdata){
+//         //const participant = { $set:{ lastStatus: Date.now()}};
+//         db.collection('participants').updateOne({name:user},{ $set:{ lastStatus: Date.now()}});
+//         res.sendStatus(200);
+//        }else{
+//         res.sendStatus(404);
+//        }
+//     });
+// });
 
-app.put("/messages/:ID_DA_MENSAGEM", (req, res) => {
-    const { user } = req.headers;
-    const promise = db.collection('participants').find({name:user}).toArray();
-    promise.then(userdata => {
-       if(userdata){
-        //const participant = { $set:{ lastStatus: Date.now()}};
-        db.collection('participants').updateOne({name:user},{ $set:{ lastStatus: Date.now()}});
-        res.sendStatus(200);
-       }else{
-        res.sendStatus(404);
-       }
-    });
-});
+// app.put("/messages/:ID_DA_MENSAGEM", (req, res) => {
+//     const { user } = req.headers;
+//     const promise = db.collection('participants').find({name:user}).toArray();
+//     promise.then(userdata => {
+//        if(userdata){
+//         //const participant = { $set:{ lastStatus: Date.now()}};
+//         db.collection('participants').updateOne({name:user},{ $set:{ lastStatus: Date.now()}});
+//         res.sendStatus(200);
+//        }else{
+//         res.sendStatus(404);
+//        }
+//     });
+// });
 
 app.listen(5000);
